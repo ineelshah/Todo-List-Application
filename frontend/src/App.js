144f782f311752561b5c2764c6413bb48ae8fec9
Neel Shah <neelshah.2798@gmail.com>
2020-11-09 12:01:08 +0530

    Frontend Added

---
+++ frontend/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodoHome from "./components/todo-home.component";
+import logo from "./logo.svg";
+
+
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+
+        <h2>MERN TODO APP</h2>
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="https://linkedin.com/in/ineelshah" target="_">
+          <img src={logo} widht="30" height="30" alt="my-todo-logo" />
+          </a>
+          <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link> 
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+            
+              <li className="navbar-item">
+               <Link to="/" className="nav-link"> Todos </Link>
+              </li>
+
+              <li className="navbar-item">
+               <Link to="/create" className="nav-link"> Create Todos </Link>
+              </li>
+
+            </ul>
+          </div>
+
+        </nav>
+
+        <Route path="/" exact component={TodoHome} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;
